0294e6a615a29ce7c4639474331efef10a8f0e2b
Shawnee M. Anderson <shawnee.m.anderson@gmail.com>
2020-08-24 20:54:54 -0400

    Update validation document.

--- validation_model1.R
+++ validation_model1.R
@@ -1,21 +1,22 @@
 ## Model Schematic 1: Identification of Partners of Incident HIV+ MSM
 ## --------------------------------------------------------------- ##
 
+suppressMessages(library("EpiModelHIV"))
 
-netstats <- readRDS("C:/Users/smande6/Box Sync/est/netstats.rda")
-epistats <- readRDS("C:/Users/smande6/Box Sync/est/epistats.rda")
-est <- readRDS("C:/Users/smande6/Box Sync/est/netest.rda")
+netstats <- readRDS("est/netstats.rda")
+epistats <- readRDS("est/epistats.rda")
+est <- readRDS("est/netest.rda")
 
 
 ## part.lookback: Rates of elicitation for partner lookback period:
 # No elicitation of partners (i.e., set lookback window to 0 time steps).
 #
-param.orig <- param_msm(epistats = epistats,
+param <- param_msm(epistats = epistats,
                    netstats = netstats,
                    hiv.scrn.rate = c(1, 1, 1),
-                   part.lookback.main = 0,
-                   part.lookback.casl = 0,
-                   part.lookback.ooff = 0,
+                   part.lookback.main = 52,
+                   part.lookback.casl = 52,
+                   part.lookback.ooff = 52,
                    part.ident.main = 1,
                    part.ident.casl = 1,
                    part.ident.ooff = 1,
@@ -25,8 +26,8 @@ param.orig <- param_msm(epistats = epistats,
 
 init <- init_msm()
 
-control <- control_msm(simno = 1,
-                       nsteps = 52*60 ,
+control <- control_msm(simno = 1001,
+                       nsteps = 52*10 ,
                        nsims = 5,
                        ncores = 1,
                        save.nwstats = FALSE,
@@ -39,22 +40,30 @@ set.seed(123)
 
 sim[[1]] <- netsim(est, param.orig, init, control)
 
-# One year partner lookback for all partnership types.
-#
-param <- param.orig
-param$part.lookback.main <- 52
-param$part.lookback.casl <- 52
-param$part.lookback.ooff <- 52
-
-sim[[2]] <- netsim(est, param, init, control)
-
 # Realistic breakdown of partnership types
 # 
+param <- param.orig
 param$part.lookback.main <- 52
 param$part.lookback.casl <- 26
 param$part.lookback.ooff <- 4
 
+sim[[2]] <- netsim(est, param, init, control)
+
+# Longer recall periods
+# 
+param$part.lookback.main <- 104
+param$part.lookback.casl <- 52
+param$part.lookback.ooff <- 26
+
 sim[[3]] <- netsim(est, param, init, control)
+
+# Very short recall periods
+# 
+param$part.lookback.main <- 12
+param$part.lookback.casl <- 6
+param$part.lookback.ooff <- 2
+
+sim[[4]] <- netsim(est, param, init, control)
 
 ## part.ident: probability of identifying eligible partner
 # No prob. of success in identification. Note: should be equivalent to sim1.
@@ -62,28 +71,28 @@ param$part.ident.main <- 0
 param$part.ident.casl <- 0
 param$part.ident.ooff <- 0
 
-sim[[4]] <- netsim(est, param, init, control)
+sim[[5]] <- netsim(est, param, init, control)
 
 # Complete partner identification
 param$part.ident.main <- 1
 param$part.ident.casl <- 1
 param$part.ident.ooff <- 1
 
-sim[[5]] <- netsim(est, param, init,control)
+sim[[6]] <- netsim(est, param, init,control)
 
 # Stratified partner identification rates - high
 param$part.ident.main <- 0.75
 param$part.ident.casl <- 0.6
 param$part.ident.ooff <- 0.3
 
-sim[[6]] <- netsim(est, param, init, control)
+sim[[7]] <- netsim(est, param, init, control)
 
 # Stratified partner identification rates - low
 param$part.ident.main <- 0.03
 param$part.ident.casl <- 0.1
 param$part.ident.ooff <- 0.05
 
-sim[[7]] <- netsim(est, param, init, control)
+sim[[8]] <- netsim(est, param, init, control)
 
 # Stratified partner identification rates - high main/casl recall,
 # low one off partner recall
@@ -91,7 +100,7 @@ param$part.ident.main <- 0.9
 param$part.ident.casl <- 0.8
 param$part.ident.ooff <- 0.05
 
-sim[[8]] <- netsim(est, param, init, control)
+sim[[9]] <- netsim(est, param, init, control)
 
 # Stratified partner identification rates - low main/casl recall,
 # high one off partner recall
@@ -99,7 +108,7 @@ param$part.ident.main <- 0.3
 param$part.ident.casl <- 0.1
 param$part.ident.ooff <- 0.8
 
-sim[[9]] <- netsim(est, param, init, control)
+sim[[10]] <- netsim(est, param, init, control)
 
 ## ptype.lookup: subset by type of relationship
 # Only main/casual
@@ -108,22 +117,27 @@ param$part.ident.casl <- 0.6
 param$part.ident.ooff <- 0.3
 param$ptype.lookup <- c(1, 2)
 
-sim[[10]] <- netsim(est, param, init, control)
+sim[[11]] <- netsim(est, param, init, control)
 
 # Only casual
 param$ptype.lookup <- 3
 
-sim[[11]] <- netsim(est, param, init, control)
+sim[[12]] <- netsim(est, param, init, control)
 
 ## hiv.scrn.rate: HIV testing rate for partners of individuas who are incident HIV+.
 # Low screen rate
 param$hiv.scrn.rate <- c(0.5, 0.25, 0.125)
 
-sim[[12]] <- netsim(est, param, init, control)
+sim[[13]] <- netsim(est, param, init, control)
+
+# No screening for identified partners
+param$hiv.scrn.rate <- c(0, 0, 0)
+
+sim[[14]] <- netsim(est, param, init, control)
 
 # High screen rate
 param$hiv.scrn.rate <- c(0.95, 0.75, 0.5)
 
-sim[[13]] <- netsim(est, param, init, control)
+sim[[14]] <- netsim(est, param, init, control)
 
 saveRDS(sim, file = "model1.sim.rda")
