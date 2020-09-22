d467ec13585fe251c488d6fb83dd9e3dfe89cfe6
smjenness <smjenness@gmail.com>
2020-09-22 10:41:13 -0400

    Change file extensions to .rds

--- estimation/estimation.R
+++ estimation/estimation.R
@@ -10,10 +10,11 @@ suppressMessages(library("ARTnet"))
 
 epistats <- build_epistats(geog.lvl = "city", geog.cat = "Atlanta", race = TRUE,
                            init.hiv.prev = rep(0.17, 3))
-saveRDS(epistats, file = "est/epistats.rda")
+saveRDS(epistats, file = "est/epistats.rds")
 netparams <- build_netparams(epistats = epistats, smooth.main.dur = TRUE)
-netstats <- build_netstats(epistats, netparams, expect.mort = 0.000478213, network.size = 20000)
-saveRDS(netstats, file = "est/netstats.rda")
+netstats <- build_netstats(epistats, netparams, expect.mort = 0.000478213, 
+                           network.size = 20000)
+saveRDS(netstats, file = "est/netstats.rds")
 
 
 # 0. Initialize Network ---------------------------------------------------
@@ -147,4 +148,4 @@ fit_inst <- netest(nw_inst,
 
 out <- list(fit_main, fit_casl, fit_inst)
 
-saveRDS(out, file = "est/netest.rda")
+saveRDS(out, file = "est/netest.rds")
