66f2f2887c2d7773886e5ddf0235b03802710e15
Shawnee M. Anderson <shawnee.m.anderson@gmail.com>
2020-10-07 13:23:11 -0400

    Update simulation file.

--- R/03-simulation.R
+++ R/03-simulation.R
@@ -7,19 +7,19 @@ suppressMessages(library("EpiModelHPC"))
 pull_env_vars()
 
 ## Parameters
-netstats <- readRDS("est/netstats.rda")
-epistats <- readRDS("est/epistats.rda")
-est <- readRDS("est/netest.rda")
+netstats <- readRDS("data/input/netstats.rda")
+epistats <- readRDS("data/input/epistats.rda")
+est <- readRDS("data/input/netest.rda")
 
 param <- param_msm(netstats = netstats,
                    epistats = epistats,
-                   hiv.test.rate = c(0.0045, 0.00425, 0.00925),
+                   hiv.test.rate = c(0.00385, 0.00380, 0.00690),
                    hiv.test.late.prob = c(0, 0, 0),
-                   tx.init.prob = c(0.1785, 0.190, 0.26),
-                   tt.part.supp = c(0.05, 0.05, 0.05),
-                   tt.full.supp = c(0.95, 0.95, 0.95),
+                   tx.init.prob = c(0.1775, 0.190, 0.2521),
+                   tt.part.supp = c(0, 0, 0),
+                   tt.full.supp = c(1, 1, 1),
                    tt.dur.supp = c(0, 0, 0),
-                   tx.halt.part.prob = c(0.005, 0.004, 0.002),
+                   tx.halt.part.prob = c(0.0062, 0.0055, 0.0031),
                    tx.halt.full.rr = c(0.45, 0.45, 0.45),
                    tx.halt.dur.rr = c(0.45, 0.45, 0.45),
                    tx.reinit.part.prob = c(0.00255, 0.00255, 0.00255),
@@ -29,7 +29,7 @@ param <- param_msm(netstats = netstats,
                    max.time.on.tx.part.int = 52 * 10,
                    max.time.off.tx.part.int = 52 * 10,
                    aids.mr = 1/250,
-                   trans.scale = c(1.8, 0.424, 0.23),
+                   trans.scale = c(2.44, 0.424, 0.270),
                    acts.scale = 1.00,
                    acts.aids.vl = 5.75,
                    prep.start = (52*60) + 1,
@@ -37,9 +37,10 @@ param <- param_msm(netstats = netstats,
                    prep.start.prob = 0.66,
                    prep.require.lnt = TRUE,
                    prep.risk.reassess.method = "year")
-
-init <- init_msm()
-
+init <- init_msm(prev.ugc = 0,
+                 prev.rct = 0,
+                 prev.rgc = 0,
+                 prev.uct = 0)
 control <- control_msm(simno = fsimno,
                        nsteps = 52*60,
                        nsims = ncores,
