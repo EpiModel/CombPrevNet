35ffc6ab6d8202b1976b495487d46811e2f142f6
Samuel Jenness <smjenness@gmail.com>
2020-10-08 16:52:06 -0400

    Reorg test files

--- scp.sh
+++ scp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Send
-scp burnin/burnin1/*.R burnin/burnin1/*.sh mox:/gscratch/csde/sjenness/combprev
+
 
 # Receive
 scp mox:/gscratch/csde/sjenness/CombPrevNet/data/input/*.rds data/input/
