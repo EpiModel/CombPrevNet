2647c31fa0ca6eaf6f84af614613add590af30a5
A. Le Guillou <aleguil@mox2.hyak.local>
2021-01-06 04:10:41 -0800

    loadR

--- loadR.sh
+++ loadR.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 . /gscratch/csde/spack/spack/share/spack/setup-env.sh
-spack load r@4.0.0
+module load r-4.0.0-gcc-9.2.0-p7wezul
+
+# spack load r-3.6.1-gcc-9.2.0-dzrq2ei 
+# spack load r@4.0.0
