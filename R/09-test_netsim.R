c67ff9dfd8d8d96b71bbe822a90ed07899ea9307
Adrien Le Guillou <adrien.leguillou@gmail.com>
2021-10-14 12:05:00 +0200

    tests

--- R/09-test_netsim.R
+++ R/09-test_netsim.R
@@ -3,25 +3,49 @@
 #
 # This file runs the model from timestep 1 using the inputs defined in
 # "R/utils-inputs.R"
-#
+
+# > gc()
+#           used (Mb) gc trigger (Mb) max used (Mb)
+# Ncells  454612 24.3     953347   51   691088 37.0
+# Vcells 1526549 11.7    8388608   64  4608561 35.2
+
 source("R/utils-inputs.R", local = TRUE)
 
+nsim <- 1
 control <- control_msm(
-  nsteps = 52 * 65,
-  nsims = 1,
-  ncores = 1,
+  nsteps = 52 * 10,
+  nsims = nsim,
+  ncores = nsim,
   truncate.el.cuml = 53,
   tracker.list = epi_trackers,
-  verbose = FALSE,
-  raw.output = FALSE
+  raw.output = FALSE,
+  verbose = FALSE
 )
 
-# sim <- netsim(orig, param, init, control)
+# > gc()
+           # used  (Mb) gc trigger  (Mb) max used  (Mb)
+# Ncells  5281792 282.1    8577165 458.1  5706747 304.8
+# Vcells 33991052 259.4   48885699 373.0 34348561 262.1
 
-options(browser = "firefox")
-profvis::profvis({
-  sim <- netsim(orig, param, init, control)
-})
+# > rm(list = ls())
+# > gc()
+#           used  (Mb) gc trigger  (Mb) max used  (Mb)
+# Ncells 2471377 132.0    6861732 366.5  5706747 304.8
+# Vcells 6088415  46.5   39108560 298.4 34348561 262.1
+
+sim <- netsim(orig, param, init, control)
+
+# gc()
+#            used  (Mb) gc trigger  (Mb) max used  (Mb)
+# Ncells  5375842 287.2   10511635 561.4 10511635 561.4
+# Vcells 35262974 269.1   63171618 482.0 63171618 482.0
+
+# > rm(list = ls())
+# > gc()
+          # used  (Mb) gc trigger  (Mb) max used  (Mb)
+# Ncells 2565156 137.0    8409308 449.2 10511635 561.4
+# Vcells 6354347  48.5   50537295 385.6 63171618 482.0
+
 
 # sim$epi <- sim$epi["num"] # keep only the "num" epi tracker
 # saveRDS(sim, "out/est/restart.rds")
--- R/z-test.R
+++ R/z-test.R
@@ -1,11 +1,41 @@
-source("R/utils-params.R", local = TRUE)
-source("R/utils-scenarios.R")
-source("R/utils-create_outcomes.R")
+source("R/utils-inputs.R", local = TRUE)
 
-scenarios <- names(c(sc_base, sc_fig3))
-scenarios_files <- paste0("out/scenarios/", scenarios, ".rds")
+control <- control_msm(
+  nsteps = 52 * 65,
+  nsims = 1,
+  ncores = 1,
+  truncate.el.cuml = 53,
+  tracker.list = epi_trackers,
 
-d_raw <- make_outcomes(scenarios_files[1], scenarios_files, scenarios)
-d_table <- make_table(d_raw)
-readr::write_csv(d_table, paste0("out/tables/t_test.csv"))
+  initialize.FUN = initialize_msm,
+  param_updater.FUN = function(dat, at) dat,
+  aging.FUN = function(dat, at) dat,
+  departure.FUN = departure_msm,
+  arrival.FUN = arrival_msm,
+  partident.FUN = function(dat, at) dat,
+  hivtest.FUN = function(dat, at) dat,
+  hivtx.FUN = function(dat, at) dat,
+  hivprogress.FUN = function(dat, at) dat,
+  hivvl.FUN = function(dat, at) dat,
+  resim_nets.FUN = simnet_msm,
+  acts.FUN = function(dat, at) dat,
+  condoms.FUN = function(dat, at) dat,
+  position.FUN = function(dat, at) dat,
+  prep.FUN = function(dat, at) dat,
+  hivtrans.FUN = function(dat, at) dat,
+  stitrans.FUN = function(dat, at) dat,
+  stirecov.FUN = function(dat, at) dat,
+  stitx.FUN = function(dat, at) dat,
+  trackers.FUN = function(dat, at) dat,
+  prev.FUN = prevalence_msm,
+  verbose.FUN = verbose.net,
 
+  verbose = FALSE,
+  raw.output = FALSE
+)
+
+options(browser = "firefox")
+
+profvis::profvis({
+  sim <- netsim(orig, param, init, control)
+})
